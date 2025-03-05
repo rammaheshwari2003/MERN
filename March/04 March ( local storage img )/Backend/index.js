f7a73457eb3443f916954db47d037ce86aff361b
Ram Maheshwari <130230374+rammaheshwari2003@users.noreply.github.com>
2025-03-05 10:01:26 +0530

    multer

--- March/04 March ( local storage img )/Backend/index.js
+++ March/04 March ( local storage img )/Backend/index.js
@@ -1,5 +1,5 @@
 const express= require("express");
-const app=express();
+const app=express(); 
 const cors= require("cors");
 const bodyParser = require('body-parser');
 const mongoose= require("mongoose");
